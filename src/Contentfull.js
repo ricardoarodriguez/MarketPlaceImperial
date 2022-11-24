ecb67d75fe322299b59d6782eeedfc788383c9bf
ricardordgz <ricardoa.rodriguez@gmail.com>
2022-11-23 22:10:05 -0500

    Ajuste con implementación del marketplace

---
+++ src/Contentfull.js
@@ -0,0 +1,10 @@
+export const Contentfull = () => {
+  const contentful = require("contentful");
+  const client = contentful.createClient({
+    space: process.env.REACT_APP_SPACE_ID,
+    environment: "master", // defaults to 'master' if not set
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  });
+
+  return client;
+};
---
+++ src/components/Animations.js
@@ -0,0 +1,4 @@
+export default function Animations() {
+  const gsap = window.gsap;
+  /* gsap.to(".heroBackground", { rotation: 27, duration: 10 }); */
+}
